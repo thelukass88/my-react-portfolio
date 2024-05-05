b201707f40fa4d070c73bf8749eeeebadb39275b
thelukass88 <l.holliday88@hotmail.com>
2024-05-06 00:52:27 +0100

    changed structure of contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <div className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
-                <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Send me a message</p>
+                <h2 className='fs-1 text-4xl sm:text-7xl font-bold inline border-b-4 border-cyan-500 text-gray-300'>Contact</h2>
+                <p className='py-4 text-2xl text-white text-center'>Let's collaborate! Feel free to drop me an email or connect through my social links.</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='mailto:l.holliday88@hotmail.com'
+            >
+                          <button className='text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center'><HiOutlineMail size={30} />l.holliday88@hotmail.com</button>
+            </a>
         </div>
     </div>
   )
